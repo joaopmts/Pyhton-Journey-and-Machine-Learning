0efa6943cf5c8bfccc8f042308bcf4acb57a2bbf
Joao Paulo <joao.p.m.rodrigues10@gmail.com>
2022-04-30 04:15:26 -0300

    v1
    Aula 1

---
+++ Aula1/calculadora.py
@@ -0,0 +1,13 @@
+print("Calculadora Ninja Plus 3000")
+v1 = int(input("Por favor, informe um numero:\n"))
+v2 = int(input("Por favor, informe outro numero:\n"))
+
+soma = v1 + v2
+sub = v1 - v2
+mult = v1 * v2
+div = v1 / v2
+
+print(f"A soma entre os numeros é: {soma}")
+print(f"A divisão entre os numeros é: {div}")
+print(f"A subtração entre os numeros é: {sub}")
+print(f"A multiplicação entre os numeros é: {mult}")
---
+++ Aula1/primeiro_programa.py
@@ -0,0 +1,3 @@
+nome = input("Por favor, informe seu nome:\n")
+
+print(f"Boa noite, {nome}")
